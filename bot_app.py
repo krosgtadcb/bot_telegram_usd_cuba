323963f8b10a9298b516f24256a2a92f252010e4
krosgtadcb <125051052+krosgtadcb@users.noreply.github.com>
2026-02-11 19:55:25 -0500

    Update bot_app.py

--- bot_app.py
+++ bot_app.py
@@ -13,7 +13,7 @@ from flask import Flask
 TELEGRAM_TOKEN = "8204621263:AAFWiXWdMH-vvRmGUb91eK45Ill_tJtRVFo"
 CHAT_ID = "-1003728489867"
 
-INTERVALO_MINUTOS = 5
+INTERVALO_MINUTOS = 50
 TIMEOUT_SEGUNDOS = 15
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
